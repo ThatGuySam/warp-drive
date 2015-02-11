a7da70601f83eb137b6db152ef61ab21196d6342
Sam Carlton <thatguy@samuelcarlton.com>
2015-02-11 16:30:43 -0600

    Web Icons

--- templates/head.php
+++ templates/head.php
@@ -13,16 +13,14 @@
 	
 	<meta name="application-name" content="Guts Church"/>
 	
-	<!-- For IE10 Metro: -->
+	<!-- For IE10 & Metro: -->
 	<meta name="msapplication-TileColor" content="#a20000">
 	<meta name="msapplication-TileImage" content="<?php echo site_url(); ?>/logo/square_com/apple-touch-icon-144x144.png">
-	
 	<meta name="msapplication-square70x70logo" content="<?php echo site_url(); ?>/logo/square/mstile-70x70.png" />
 	<meta name="msapplication-square150x150logo" content="<?php echo site_url(); ?>/logo/square/mstile-150x150.png" />
 	<meta name="msapplication-wide310x150logo" content="<?php echo site_url(); ?>/logo/square/mstile-310x150.png" />
 	<meta name="msapplication-square310x310logo" content="<?php echo site_url(); ?>/logo/square_com/mstile-310x310.png" />
 	<meta name="msapplication-notification" content="frequency=30;polling-uri=http://notifications.buildmypinnedsite.com/?feed=http://gutschurch.com/feed&amp;id=1;polling-uri2=http://notifications.buildmypinnedsite.com/?feed=http://gutschurch.com/feed&amp;id=2;polling-uri3=http://notifications.buildmypinnedsite.com/?feed=http://gutschurch.com/feed&amp;id=3;polling-uri4=http://notifications.buildmypinnedsite.com/?feed=http://gutschurch.com/feed&amp;id=4;polling-uri5=http://notifications.buildmypinnedsite.com/?feed=http://gutschurch.com/feed&amp;id=5;cycle=1" />
-	
 	
 	<!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
 	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo site_url(); ?>/logo/square/apple-touch-icon-152x152.png">
@@ -41,7 +39,6 @@
 	
 	<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
 	<link rel="apple-touch-icon-precomposed" href="<?php echo site_url(); ?>/logo/square/apple-touch-icon-57x57.png">
-	
 	
 	<?php wp_head(); ?>
 	
