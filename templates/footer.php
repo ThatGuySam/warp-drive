3a0c8e93491389f5022edd0159ffbf30814c4bc1
Sam Carlton <thatguy@samuelcarlton.com>
2015-03-03 13:12:34 -0600

    Removed "New" form feedback

--- templates/footer.php
+++ templates/footer.php
@@ -8,7 +8,7 @@
   </div>
   
   <div class="feedback" style="text-align: center; padding: 1em; font-size: 0.75em;">
-	  <a href="http://form.jotformpro.com/form/30093898678977" target="_blank" >New Website Feedback</a>
+	  <a href="http://form.jotformpro.com/form/30093898678977" target="_blank" >Website Feedback</a>
   </div>
   
 </footer>
