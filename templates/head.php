fa6ed100880c7c1c3726cfbed2873910d994f97c
Sam Carlton <thatguy@samuelcarlton.com>
2015-04-23 09:08:06 -0500

    Added CSS Fix for mobile

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Prevent text size adjustment on orientation change. -->
+	<style>html { -webkit-text-size-adjust: 100%; }</style>
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
     <?php wp_head(); ?>
   </head>
