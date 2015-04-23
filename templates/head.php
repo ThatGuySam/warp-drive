cad9ca2c13aa39d7d5b534e3a796a7f6df7630e6
Sam Carlton <thatguy@samuelcarlton.com>
2015-04-23 09:09:10 -0500

    Added Site Icons

--- templates/head.php
+++ templates/head.php
@@ -9,5 +9,30 @@
     
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
+    
+    <meta name="application-name" content="Tougher Than Hell"/>
+    
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo site_url(); ?>/icons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo site_url(); ?>/icons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo site_url(); ?>/icons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo site_url(); ?>/icons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo site_url(); ?>/icons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo site_url(); ?>/icons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo site_url(); ?>/icons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo site_url(); ?>/icons/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="<?php echo site_url(); ?>/icons/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="<?php echo site_url(); ?>/icons/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="<?php echo site_url(); ?>/icons/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="<?php echo site_url(); ?>/icons/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="<?php echo site_url(); ?>/icons/favicon-128.png" sizes="128x128" />
+	<meta name="application-name" content="Tougher Than Than"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="<?php echo site_url(); ?>/icons/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="<?php echo site_url(); ?>/icons/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="<?php echo site_url(); ?>/icons/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="<?php echo site_url(); ?>/icons/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="<?php echo site_url(); ?>/icons/mstile-310x310.png" />
+	<meta name="msapplication-notification" content="frequency=30;polling-uri=http://notifications.buildmypinnedsite.com/?feed=<?php echo site_url(); ?>/icons/rss&amp;id=1;polling-uri2=http://notifications.buildmypinnedsite.com/?feed=<?php echo site_url(); ?>/icons/rss&amp;id=2;polling-uri3=http://notifications.buildmypinnedsite.com/?feed=<?php echo site_url(); ?>/icons/rss&amp;id=3;polling-uri4=http://notifications.buildmypinnedsite.com/?feed=<?php echo site_url(); ?>/icons/rss&amp;id=4;polling-uri5=http://notifications.buildmypinnedsite.com/?feed=<?php echo site_url(); ?>/icons/rss&amp;id=5;cycle=1" />
+    
     <?php wp_head(); ?>
   </head>
