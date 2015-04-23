4bd29e9392b384bb8d2ef01c4f3904fd18ba1413
Sam Carlton <thatguy@samuelcarlton.com>
2015-04-23 09:08:41 -0500

    Added more options to viewport

--- templates/head.php
+++ templates/head.php
@@ -3,9 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
     <!-- Prevent text size adjustment on orientation change. -->
 	<style>html { -webkit-text-size-adjust: 100%; }</style>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
     <link rel="alternate" type="application/rss+xml" title="<?= get_bloginfo('name'); ?> Feed" href="<?= esc_url(get_feed_link()); ?>">
     <?php wp_head(); ?>
   </head>
