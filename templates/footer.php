56bf99941a1f1d9887215beae31e843912d7c2ec
Sam Carlton <thatguy@samuelcarlton.com>
2015-06-19 20:51:09 -0500

    Removed Website Feedback from footer

--- templates/footer.php
+++ templates/footer.php
@@ -7,8 +7,4 @@
 	  © Guts Church <?php echo date('Y'); ?>
   </div>
   
-  <div class="feedback" style="text-align: center; padding: 1em; font-size: 0.75em;">
-	  <a href="http://form.jotformpro.com/form/30093898678977" target="_blank" >Website Feedback</a>
-  </div>
-  
 </footer>
