5501afef7fce5a8d462ba57e3dbedc795880a7fe
Sam Carlton <thatguy@samuelcarlton.com>
2015-09-04 18:12:46 -0500

    Hide sidebar for pages, posts & events

--- lib/config.php
+++ lib/config.php
@@ -54,6 +54,9 @@ function display_sidebar() {
       [
         'is_404',
         'is_front_page',
+        'is_page',
+		'is_single',
+		'is_event',
         ['is_page_template', 'template-custom.php']
       ]
     );
