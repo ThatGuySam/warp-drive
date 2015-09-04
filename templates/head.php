b161572462cf79def2fac8a94a5b4d02097fd6b2
Sam Carlton <thatguy@samuelcarlton.com>
2015-09-04 18:27:01 -0500

    Added test size adjustment fix

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,9 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
   
+  <!-- Prevent text size adjustment on orientation change. -->
+  <style>html { -webkit-text-size-adjust: 100%; }</style>
+  
   <meta name="application-name" content="<?php bloginfo( 'name' ) ?>"/>
   
   <?php wp_head(); ?>
